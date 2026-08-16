7a8944db05c1af3fb2726453dc96853391ceada8
kenkenpa7 <beachclean20112@gmail.com>
2026-08-16 11:40:21 +0900

    feat: Add studioHost 'world777' to sanity.cli.js

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: 'yn5o3m70',
     dataset: 'production'
   },
+  studioHost: 'world777',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
